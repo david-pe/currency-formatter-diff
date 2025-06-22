824382124f72a6f031044d8ada3a77b2968b5bed
David Perlman <davidp@wix.com>
2025-06-22 18:02:16 +0300

    feat: add sparticuz/chromium for currency formatting

--- worker.js
+++ worker.js
@@ -1,16 +1,19 @@
 const { workerData, parentPort } = require('worker_threads');
-const { formatCurrencyJava, formatCurrencyNode } = require('./format-currency');
+const { formatCurrencyJava, formatCurrencyNode, formatCurrencySparticuz } = require('./format-currency');
 
 const { currency, locales, value } = workerData;
 
-for (const locale of locales) {
-    const javaCldr = formatCurrencyJava(locale, currency, value, true);
-    const javaLegacy = formatCurrencyJava(locale, currency, value, false);
-    const node = formatCurrencyNode(locale, currency, value);
+(async () => {
+    for (const locale of locales) {
+        const javaCldr = formatCurrencyJava(locale, currency, value, true);
+        const javaLegacy = formatCurrencyJava(locale, currency, value, false);
+        const node = formatCurrencyNode(locale, currency, value);
+        const sparticuz = await formatCurrencySparticuz(locale, currency, value);
 
-    parentPort.postMessage({
-        currency,
-        locale,
-        results: { javaCldr, javaLegacy, node },
-    });
-} 
+        parentPort.postMessage({
+            currency,
+            locale,
+            results: { javaCldr, javaLegacy, node, sparticuz },
+        });
+    }
+})();
